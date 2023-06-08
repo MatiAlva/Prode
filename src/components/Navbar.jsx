aa422dce21e1710b8a78ea2bdd9b4a4b58b43be3
TobysNap <naptobys17@gmail.com>
2023-06-08 08:23:49 -0300

    Navbar componente y estilos

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import '../scss/Navbar.scss';
+
+function Navbar() {
+	return (
+		<header className="primary-header flex">
+			<a className="navlink brand fw-700">PRODE</a>
+			<nav>
+				<ul className="navbar flex">
+					<li>
+						<a className="navlink">Inicio</a>
+					</li>
+					<li>
+						<a className="navlink active">Partidos</a>
+					</li>
+					<li>
+						<a className="navlink">Ranking</a>
+					</li>
+					<li>
+						<a className="navlink">Grupos</a>
+					</li>
+					<li>
+						<a className="navlink">FAQ</a>
+					</li>
+					<li>
+						<a className="btn btn--primary">Logout</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default Navbar;
